c606a1df594b5f3a33cccdda1be4869ec8b2fd36
medeirosrebeca <110829343+medeirosrebeca@users.noreply.github.com>
2024-01-01 23:53:06 +0000

    atual

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -33,11 +33,11 @@ namespace DesafioFundamentos.Models
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
-
+           
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
-
+            string placa = Console.ReadLine().ToUpper();
+           
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
@@ -46,13 +46,17 @@ namespace DesafioFundamentos.Models
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                if (int.TryParse(Console.ReadLine(), out int horas) && horas >= 0)
+                {
+                decimal valorTotal = this.precoInicial + (this.precoPorHora * horas);
 
+        
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
+                veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                }
             }
             else
             {
@@ -67,6 +71,10 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
+                foreach (var veiculo in veiculos)
+                {
+                Console.WriteLine(veiculo);
+                }
                 // *IMPLEMENTE AQUI*
             }
             else
